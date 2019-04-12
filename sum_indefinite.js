3c9fe875e6e7811c504af322155933d048379bcb
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-12 02:05:31 -0700

    test for: valid array elements passed

--- sum_indefinite.js
+++ sum_indefinite.js
@@ -1,4 +1,7 @@
 function sum_indefinite(arr){
+    if(Array.isArray(arr) === true){
+        return arr.reduce((a, b) => a + b, 0);
+    }
     return 'Wrong input';
 }
 
--- sum_indefinite.test.js
+++ sum_indefinite.test.js
@@ -4,3 +4,10 @@ test('No input parameter;input is not an array;empty array',()=>{
     expect(indefinite_sum()).toBe('Wrong input');
 });
 
+test('Valid Array is passed',()=>{
+    expect(indefinite_sum([1,2,3])).toBe(6);
+});
+
+test('One or more parameters are invalid',()=>{
+    expect(indefinite_sum([1,null,undefined])).toBe('Wrong input');
+})
